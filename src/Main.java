da803df4eaeb34c5cf23c1c23ce94f4c18cf2248
Varsha_bala <balava@mail.uc.edu>
2022-09-22 15:00:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 12;
+
+        if( age >= 21 )
+        {
+            System.out.println("You get a wristband!");
+        }
+        else
+        {
+            System.out.println("Sorry, you don't get a wristband.You're too young!");
+        }
+    }
+}
